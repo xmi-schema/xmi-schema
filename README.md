5eb7f9472099ea7a375d76057ac44fbc1d41820a
silentx <darellchua2@gmail.com>
2023-10-02 17:00:18 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+I used the followinfg link for reference
+https://medium.com/analytics-vidhya/how-to-create-a-python-library-7d5aea80cc3f
---
+++ tests/test_myfunctions.py
@@ -0,0 +1,8 @@
+from schema import myfunctions
+
+
+def test_haversine():
+    # Amsterdam to Berlin
+    assert myfunctions.haversine(
+        4.895168, 52.370216, 13.404954, 52.520008
+    ) == 576.6625818456291
