b70ed0fef8e95e01e6d2ad9f8d8fff57874bc7ed
silentx <darellchua2@gmail.com>
2023-10-12 17:54:02 +0800

    update

--- tests/xmi/v1/test_scripts/test_xmi_structural_cross_section.py
+++ tests/xmi/v1/test_scripts/test_xmi_structural_cross_section.py
@@ -8,7 +8,7 @@ import json
 
 
 def test_xmi_structural_cross_section_ok_1():
-    json_path = "tests/xmi/v1/test_inputs/xmi_structural_material/xmi_parser_structural_material_only.json"
+    json_path = "tests/xmi/v1/test_inputs/xmi_structural_cross_section/xmi_structural_material_test.json"
     with open(json_path, 'r') as f:
         data = json.load(f)
 
@@ -54,7 +54,7 @@ def test_xmi_structural_cross_section_ok_1():
 
 
 def test_xmi_structural_cross_section_fail():
-    json_path = "tests/xmi/v1/test_inputs/xmi_structural_material/xmi_parser_structural_material_only.json"
+    json_path = "tests/xmi/v1/test_inputs/xmi_structural_cross_section/xmi_structural_material_test.json"
     with open(json_path, 'r') as f:
         data = json.load(f)
 
@@ -93,3 +93,55 @@ def test_xmi_structural_cross_section_fail():
             parameters=parameters
         )
         print(xmi_structural_cross_section)
+
+
+def test_xmi_structural_cross_section_ok_2():
+    json_path = "tests/xmi/v1/test_inputs/xmi_structural_cross_section/xmi_structural_cross_section_test.json"
+    with open(json_path, 'r') as f:
+        data = json.load(f)
+    structural_material_dict = data['StructuralMaterial'][0]
+    structural_cross_section_dict = data['StructuralCrossSection'][0]
+
+    id = structural_material_dict['ID']
+    name = structural_material_dict['Name']
+    description = structural_material_dict['Description']
+    ifcguid = structural_material_dict['IFCGUID']
+    material_type = XmiStructuralMaterialTypeEnum.from_attribute_get_enum(
+        structural_material_dict['Type'])
+    grade = structural_material_dict['Grade']
+    unit_weight = structural_material_dict['UnitWeight']
+    e_modulus = structural_material_dict['EModulus']
+    g_modulus = structural_material_dict['GModulus']
+    poisson_ratio = structural_material_dict['PoissonRatio']
+    thermal_coefficient = structural_material_dict['ThermalCoefficient']
+
+    xmi_structural_material = XmiStructuralMaterial(
+        id=id,
+        name=name,
+        description=description,
+        ifcguid=ifcguid,
+        material_type=material_type,
+        grade=grade,
+        unit_weight=unit_weight,
+        e_modulus=e_modulus,
+        g_modulus=g_modulus,
+        poisson_ratio=poisson_ratio,
+        thermal_coefficient=thermal_coefficient
+    )
+
+    expected_shape = XmiStructuralCrossSectionShapeEnum.from_attribute_get_enum(
+        structural_cross_section_dict['Shape'])
+
+    expected_parameters = tuple(
+        structural_cross_section_dict['Parameters'].split(";"))
+    xmi_structural_cross_section = XmiStructuralCrossSection(
+        material=xmi_structural_material,
+        shape=expected_shape,
+        parameters=expected_parameters
+    )
+
+    assert isinstance(xmi_structural_cross_section, XmiStructuralCrossSection)
+    assert xmi_structural_cross_section.Material == xmi_structural_material
+    assert xmi_structural_cross_section.Parameters == expected_parameters
+    assert xmi_structural_cross_section.Shape == expected_shape
+    assert isinstance(xmi_structural_cross_section.Parameters, tuple)
